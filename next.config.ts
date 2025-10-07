33998fd4f3ac4354eb30af30065ecdc85a361150
starkspartacus <hnm7877@gmail.com>
2025-10-07 16:53:50 +0200

    Update Next.js configuration by adding a comment for clarity on existing options.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   //output: 'export', // Génère un site statique prêt pour Azure Static Web Apps
-  // autres options que tu avais déjà
+  // autres options que tu avais déjà//
 }
 
 export default nextConfig
